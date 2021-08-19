eb0fb0220cb42fb1f2f1b0b7499300fc5c2fd8d9
Ghiri <ghirihari@gmail.com>
2021-08-19 13:58:42 +0530

    Empty Data Compatatibility

--- src/App.js
+++ src/App.js
@@ -8,17 +8,28 @@ export default class App extends React.Component {
     super();
     // Clock
     var thedate = new Date(); 
-    // Data from LS
-    let LocalData = JSON.parse(localStorage.data)
 
     this.state={
       currentMinute : (thedate.getHours()*60)+(thedate.getMinutes()),
-      day : LocalData.Day,
-      subjects : LocalData.Subject,
-      schedule : LocalData.Schedule,
+      day : ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday','Sunday'],
+      subjects : [],
+      schedule : [],
     }
   }
   
+  reloadData = () => {
+    let LocalData = localStorage.data;
+    if(LocalData){
+      LocalData = JSON.parse(LocalData)
+      this.setState({
+        subjects : LocalData.Subject,
+        schedule : LocalData.Schedule
+      })
+    }
+  }
+  componentDidMount = () => {
+    this.reloadData();
+  }
   resetLocalData = () => {
     const _days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday','Sunday']
     const _subjects = {
@@ -82,7 +93,7 @@ export default class App extends React.Component {
   }
 
   deletePeriod = (Day,Id,StartTime,EndTime) => {
-    console.log(Day,Id,StartTime,EndTime)
+    // console.log(Day,Id,StartTime,EndTime)
     // const old = JSON.parse(JSON.stringify(this.state.schedule))
     // old[Day].period.push({id:Id,time:{start:StartTime,end:EndTime}})
     // this.setState({schedule:old})
@@ -97,7 +108,7 @@ export default class App extends React.Component {
   }
 
   render(){
-    this.resetLocalData();
+    // this.resetLocalData();
     let data = {
       Day:this.state.day, 
       Subject:this.state.subjects, 
@@ -106,7 +117,8 @@ export default class App extends React.Component {
       appendSubjects:this.appendSubjects, 
       setSubjects:this.setSubjects, 
       setSchedule:this.setSchedule,
-      Timer:this.timer 
+      Timer:this.timer,
+      reloadData:this.reloadData
     }
 
     return (
@@ -116,3 +128,5 @@ export default class App extends React.Component {
     );
   }
   }
+
+  // {"Day":["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],"Subject":{"0":{"name":"Placement","faculty":"Nil"},"1":{"name":"SCH","faculty":"Nil"},"2":{"name":"TWM","faculty":"Dr.M.Umarani"},"15MSS91":{"name":"Information Security","type":"class","meet":"https://meet.google.com/lookup/cgecmskjvs","faculty":"Dr.P.Aruna"},"15MSS92":{"name":"Professional Ethics","type":"class","meet":"https://meet.google.com/rgy-ipzm-fxd","faculty":"Dr.A.Saravanan"},"15MSSE01":{"name":"Software User Interface Design","type":"class","meet":"https://meet.google.com/lookup/f5sh5wdffb","faculty":"Ms.V.Shanthi"},"15MSSE38":{"name":"Quantum Computing","type":"class","meet":"https://meet.google.com/lookup/buh37uk5aw","faculty":"Dr.S.Manjula Gandhi"},"15MSSE27":{"name":"Computer Vision","type":"class","meet":"https://meet.google.com/lookup/brgxg34cy3","faculty":"Dr.M.Umarani"},"15MSS93":{"name":"Information Security Lab","type":"lab","meet":"https://meet.google.com/lookup/agvs77wh4n","faculty":"Dr.A.Kannamal"},"15MSSL15":{"name":"Quantum Computing Lab","type":"lab","meet":"https://meet.google.com/cni-wfpk-dfr","faculty":"Dr.S.Manjula Gandhi"},"15MSSL08":{"name":"Image Processing Lab","type":"lab","meet":"https://meet.google.com/lookup/crolo2dqdw","faculty":"Ms.V.Shanthi"}},"Schedule":[{"day":"Monday","period":[{"no":1,"id":"15MSS91","time":{"start":"9:00","end":"10:45"}},{"no":2,"id":"15MSSE27","time":{"start":"11:00","end":"12:45"}},{"no":3,"id":"15MSS93","time":{"start":"14:00","end":"16:45"}}]},{"day":"Tuesday","period":[{"no":1,"id":"15MSS92","time":{"start":"9:00","end":"9:50"}},{"no":2,"id":"15MSSL08","time":{"start":"9:55","end":"12:45"}},{"no":3,"id":"15MSSE01","time":{"start":"14:00","end":"14:50"}},{"no":4,"id":"15MSSE38","time":{"start":"14:55","end":"15:45"}},{"no":5,"id":"1","time":{"start":"15:55","end":"16:45"}}]},{"day":"Wednesday","period":[{"no":1,"id":"15MSSE27","time":{"start":"9:00","end":"9:50"}},{"no":2,"id":"15MSS91","time":{"start":"9:55","end":"10:45"}},{"no":3,"id":"15MSSL08","time":{"start":"11:00","end":"12:45"}},{"no":4,"id":"15MSSL15","time":{"start":"14:00","end":"16:45"}}]},{"day":"Thursday","period":[{"no":1,"id":"15MSSE01","time":{"start":"9:00","end":"10:45"}},{"no":2,"id":"15MSSL15","time":{"start":"11:00","end":"12:45"}},{"no":3,"id":"0","time":{"start":"14:00","end":"16:45"}}]},{"day":"Friday","period":[{"no":1,"id":"15MSSE38","time":{"start":"9:00","end":"10:45"}},{"no":2,"id":"15MSS93","time":{"start":"11:00","end":"12:45"}},{"no":3,"id":"15MSS92","time":{"start":"14:00","end":"15:45"}},{"no":4,"id":"2","time":{"start":"15:55","end":"16:45"}}]}]}
--- src/Settings.js
+++ src/Settings.js
@@ -1,19 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import DataContext from './DataContext';
 
 // Matrial UI
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 const Settings = () => {
+    const data = useContext(DataContext);   
     const [User, setUser] = useState(localStorage.user)
-    const [Data, setData] = useState(null);
+    const [Data, setData] = useState("");
     
     const addAuthUser = () => {
         localStorage.user = User;
     }
 
     const addData = () => {
-        localStorage.Data = Data;
+        localStorage.data = Data;
+        data.reloadData()
     }
 
     return (
@@ -25,7 +28,7 @@ const Settings = () => {
                         <Button onClick={()=>addAuthUser()} variant="contained" color="secondary" style={{margin:'10px'}}>Add</Button>
                     </div>
                     <div>
-                        <TextField value={Data} onChange={(e)=>setData(e.target.value)} color="secondary" variant="filled" InputProps={{style: { color: '#fff' }}} InputLabelProps={{style: { color: '#fff' }}} margin="dense" label="Set Auth User" />
+                        <TextField value={Data} onChange={(e)=>setData(e.target.value)} color="secondary" variant="filled" InputProps={{style: { color: '#fff' }}} InputLabelProps={{style: { color: '#fff' }}} margin="dense" label="Add Data" />
                         <Button onClick={()=>addData()} variant="contained" color="secondary" style={{margin:'10px'}}>Add</Button>
                     </div>
                 </div>
